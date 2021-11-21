016642ee2110f037e93988ff9168a3911bf1f5e1
Artem Sitnikov <artem.sitnikov@exness.com>
2021-11-21 19:06:08 +0200

    refactor: eslint monitors cypress folder

--- cypress/integration/core/utils.spec.ts
+++ cypress/integration/core/utils.spec.ts
@@ -1,52 +1,52 @@
-import { getBasePath } from 'core/utils';
+import { getBasePath } from 'core/utils'
 
 describe('Utils', () => {
   describe('getBasePath', () => {
-    const { APP_BASE_PATH } = global.window;
-    const { process } = global;
+    const { APP_BASE_PATH } = global.window
+    const { process } = global
 
     afterEach(() => {
-      global.window.APP_BASE_PATH = APP_BASE_PATH;
-      global.process = process;
-    });
+      global.window.APP_BASE_PATH = APP_BASE_PATH
+      global.process = process
+    })
 
-    it('getBasePath returns basepath from window for browser', () => {
-      global.window.APP_BASE_PATH = '/some-path-from-window';
+    it('getBasePath returns basePath from window for browser', () => {
+      global.window.APP_BASE_PATH = '/some-path-from-window'
       global.process = {
         env: {
           BROWSER: true,
           APP_BASE_PATH: '/some-path-from-env',
         },
-      };
+      }
 
-      const result = getBasePath();
-      expect(result).to.equal('/some-path-from-window');
-    });
+      const result = getBasePath()
+      expect(result).to.equal('/some-path-from-window')
+    })
 
-    it('getBasePath returns basepath from env for node', () => {
-      global.window.APP_BASE_PATH = '/some-path-from-window';
+    it('getBasePath returns basePath from env for node', () => {
+      global.window.APP_BASE_PATH = '/some-path-from-window'
       global.process = {
         env: {
           BROWSER: false,
           APP_BASE_PATH: '/some-path-from-env',
         },
-      };
+      }
 
-      const result = getBasePath();
-      expect(result).to.equal('/some-path-from-env');
-    });
+      const result = getBasePath()
+      expect(result).to.equal('/some-path-from-env')
+    })
 
-    it('getBasePath returns empty string if no basepath', () => {
-      global.window.APP_BASE_PATH = '';
+    it('getBasePath returns empty string if no basePath', () => {
+      global.window.APP_BASE_PATH = ''
       global.process = {
         env: {
           BROWSER: false,
           APP_BASE_PATH: '',
         },
-      };
+      }
 
-      const result = getBasePath();
-      expect(result).to.equal('');
-    });
-  });
-});
+      const result = getBasePath()
+      expect(result).to.equal('')
+    })
+  })
+})
